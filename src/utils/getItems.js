4c3b7d715b8285eaaeff02b085930dfe67d85cc0
Elton <28992495+Lucrum@users.noreply.github.com>
2024-02-24 17:46:13 -0500

    Remove debug print

--- src/utils/getItems.js
+++ src/utils/getItems.js
@@ -3,7 +3,6 @@
 async function getStoreItems(setItems) {
   const response = await fetch("https://fakestoreapi.com/products");
   const data = await response.json();
-  console.log(data);
   setItems(data);
 }
 
