04fb65ac676a44e22f5b6b9174bb323e2931973c
Elton <28992495+Lucrum@users.noreply.github.com>
2024-02-27 14:01:52 -0500

    Price information

--- src/components/shop/ItemDisplay.jsx
+++ src/components/shop/ItemDisplay.jsx
@@ -4,10 +4,11 @@ function ItemDisplay({ item }) {
   return (
     <div className="flex border-gray-600 border-8 h-96 rounded-xl">
       <img className="w-72 object-contain" src={item.image} />
-      <div className="flex flex-col bg-gray-300 rounded-r">
-        <h4 className="text-3xl m-8 text-left">{item.title}</h4>
-        <span className="text-lg m-8 text-left">{item.description}</span>
-        <span className="ml-8 text-left">
+      <div className="flex flex-col bg-gray-300 rounded-r p-8">
+        <h4 className="text-3xl text-left">{item.title}</h4>
+        <span className="text-left">{`$${item.price.toFixed(2)}`}</span>
+        <span className="my-8 text-lg text-left">{item.description}</span>
+        <span className="my-8 text-left">
           Rating: {item.rating.rate} from {item.rating.count} reviews
         </span>
       </div>
